d87240e348277033918423ec6953653d7a5e3f9d
Juan Esteban Vila Marín <jvilamarin31@gmail.com>
2025-10-10 13:36:06 -0500

    Add files via upload

---
+++ ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/ServiCiudadCaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServiCiudadCali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiCiudadCaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiCiudadCaliApplication.class, args);
+	}
+
+}
---
+++ ServiCiudadCali/src/test/java/com/example/ServiCiudadCali/ServiCiudadCaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiCiudadCali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiCiudadCaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
