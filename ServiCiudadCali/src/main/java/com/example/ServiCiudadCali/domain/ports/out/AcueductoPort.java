89b750be5dc31c8ca18c631991c324e90ba8a57f
Katar012 <juandandlinda@gmail.com>
2025-10-18 15:04:34 -0500

    Aplicar nuevo .gitignore

--- ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/domain/ports/out/AcueductoPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.ServiCiudadCali.domain.ports.out;
-
-import com.example.ServiCiudadCali.domain.model.FacturaAcueducto;
-
-import java.util.Optional;
-
-public interface AcueductoPort {
-    Optional<FacturaAcueducto> consultarPorClienteId(String clienteId);
-}
--- ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/domain/ports/out/EnergiaPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.ServiCiudadCali.domain.ports.out;
-
-import com.example.ServiCiudadCali.domain.model.FacturaEnergia;
-
-import java.util.Optional;
-
-public interface EnergiaPort {
-    Optional<FacturaEnergia> consultarPorClienteId(String clienteId);
-}
