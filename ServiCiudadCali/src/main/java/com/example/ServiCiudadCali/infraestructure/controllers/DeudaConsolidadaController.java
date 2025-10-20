33fdf1d72a04c59a83a4d6bd7d61f056648b5ead
Katar012 <juandandlinda@gmail.com>
2025-10-20 12:15:00 -0500

    .

--- ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/infraestructure/controllers/DeudaConsolidadaController.java
+++ ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/infraestructure/controllers/DeudaConsolidadaController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/clientes")
+@RequestMapping("/api/clientes")
 public class DeudaConsolidadaController {
 
     private final ConsultarFacturasClienteUseCaseImpl consultarFacturasClienteUseCase;
