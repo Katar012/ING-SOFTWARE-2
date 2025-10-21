733ecd826b8a0aa8c260be8400f2bc40b4c41c4b
Katar012 <juandandlinda@gmail.com>
2025-10-20 20:05:33 -0500

    Pasar el ejecutable al mismo nivel application, el pom no recibe mas de 3 entradas en consumos_energia.txt

--- ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/ServiCiudadCaliApplication.java
+++ ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/ServiCiudadCaliApplication.java
@@ -1,4 +1,4 @@
-package com.example.ServiCiudadCali.application;
+package com.example.ServiCiudadCali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
