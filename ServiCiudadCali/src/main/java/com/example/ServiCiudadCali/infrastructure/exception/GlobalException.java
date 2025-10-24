f209ab94ed4bee923a52d938d4f3e65117ac478e
jvilamarin31 <jvilamarin31@gmail.com>
2025-10-23 23:19:22 -0500

    Agregue un metodo mas de excepcion

--- ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/infrastructure/exception/GlobalException.java
+++ ServiCiudadCali/src/main/java/com/example/ServiCiudadCali/infrastructure/exception/GlobalException.java
@@ -8,8 +8,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalException {
+
+    // Metodo de la capa de dominio de la logica de negocio para manejar excepciones de recurso no encontrado
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
+
+    // Maneja cualquier error no controlado
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
