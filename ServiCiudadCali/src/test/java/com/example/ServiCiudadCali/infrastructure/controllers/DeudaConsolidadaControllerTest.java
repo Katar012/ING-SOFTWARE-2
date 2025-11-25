30e37ceae7a162e5cf16c8871dd9d776c6a97e2e
Juan Esteban Vila Marín <jvilamarin31@gmail.com>
2025-11-25 14:09:22 -0500

    Ya funcionan los 75 test implementados con la IA del profesor.

--- ServiCiudadCali/src/test/java/com/example/ServiCiudadCali/infrastructure/controllers/DeudaConsolidadaControllerTest.java
+++ ServiCiudadCali/src/test/java/com/example/ServiCiudadCali/infrastructure/controllers/DeudaConsolidadaControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.ServiCiudadCali.application.dto.DeudaConsolidadaDTO;
@@ -24,6 +25,7 @@ import com.example.ServiCiudadCali.domain.exception.ResourceNotFoundException;
 import com.example.ServiCiudadCali.domain.ports.in.ConsultarFacturasClienteUseCase;
 
 @WebMvcTest(DeudaConsolidadaController.class)
+@ActiveProfiles("test") 
 class DeudaConsolidadaControllerTest {
 
     @Autowired
